baa86d8989bfd75f52811a7639b2b7d7a4c9e43e
deut-erium <himanshuthesheoran@gmail.com>
2022-10-25 01:34:22 +0530

    Add twist and shout zh3r0 ctf v2

---
+++ Zh3r0_CTF_V2_2021-Twist_and_Shout/server_files/secret.py
@@ -0,0 +1,1 @@
+flag = b"Here is the flag you might be looking for my friend zh3r0{7h3_fu7ur3_m1gh7_b3_c4p71v471ng_bu7_n0w_y0u_kn0w_h0w_t0_l00k_a7_7h3_p457} Why am i making it so long? no reason particularly :P"
